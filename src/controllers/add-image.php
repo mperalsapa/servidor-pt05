d846d244317014a28ae11eaa59caa96f5455e215
Marc Peral <m.peral@sapalomera.cat>
2022-11-16 15:53:58 +0100

    feat[gallery]: added upload file to server file system

--- src/controllers/add-image.php
+++ src/controllers/add-image.php
@@ -1,11 +1,62 @@
 <?php
 // Marc Peral
 // script que s'encarrega de mostrar la galeria d'imatges
+
+// la funcio de pujar una imatge s'ha "inspirat" en aquest tutorial de w3schools
+// https://www.w3schools.com/php/php_file_upload.asp
+
 require_once("src/internal/viewFunctions/pagging.php");
 require_once("src/internal/db/session_manager.php");
+include_once("src/internal/viewFunctions/form-error.php");
+include_once("src/internal/viewFunctions/browser.php");
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    include("env.php");
+    $targetFile = $uploadsFolder . $_FILES["imageFile"]["name"];
+    $uploadOk = 1;
+    $imageFileType = strtolower(pathinfo($targetFile, PATHINFO_EXTENSION));
+
+    // comprovem si el post rebut es des d'un formulari amb un submit
+    if (isset($_POST["submit"])) {
+        $check = getimagesize($_FILES["imageFile"]["tmp_name"]);
+        if ($check == false) {
+            returnAlert("El fixter que s'ha introduit no es una imatge.", "danger", "src/views/add-image.vista.php", null);
+        }
+    } else {
+        returnAlert("El fixter que s'ha introduit no es una imatge.", "danger", "src/views/add-image.vista.php", null);
+    }
+
+
+    // comprovem si el fitxer que volem guardar ja existeix (o un fitxer amb el mateix nom)
+    if (file_exists($targetFile)) {
+        returnAlert("El nom del fixer ja existeix. Canvia el nom i torna a provar.", "danger", "src/views/add-image.vista.php", null);
+    }
+
+
+    // comprovem si el fitxer es menor del tamany maxim
+    if ($_FILES["imageFile"]["size"] > 500000) {
+        returnAlert("El fitxer es massa gran.", "danger", "src/views/add-image.vista.php", null);
+    }
 
 
 
+    // comprovem si el fixer te una extensio d'imatge (jpg, png, jpeg o gif)
+    if (
+        $imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+        && $imageFileType != "gif"
+    ) {
+        returnAlert("El fitxer no es una imatge. Els fitxers admesos son: jpg, jpeg, png i gif..", "danger", "src/views/add-image.vista.php", null);
+    }
 
 
-include_once("src/views/add-image.vista.php");
+    // intentem guardar la imatge, si dona error mostrem un misatge
+    if (move_uploaded_file($_FILES["imageFile"]["tmp_name"], $targetFile)) {
+        redirectClient("gallery");
+    } else {
+        returnAlert("El fitxer no s'ha pogut guardar. Torna a provar mes tard. Si l'error persisteix, contacta amb un administrador.", "danger", "src/views/add-image.vista.php", null);
+    }
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    include_once("src/views/add-image.vista.php");
+}
--- src/views/add-image.vista.php
+++ src/views/add-image.vista.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?= $viewData["formTitle"] ?></title>
+    <title>Afegir Imatge</title>
     <?php include_once("src/internal/viewFunctions/header.php"); ?>
 </head>
 
@@ -15,7 +15,7 @@
     ?>
     <div class="d-flex align-items-center justify-content-center">
         <div class="bg-white rounded col-10 col-md-8 col-lg-6 col-xxl-4 mb-4">
-            <form class="align-middle m-4" action="write-article<?php echo $viewData["id"] ? "?id=" . $viewData["id"] : "" ?>" method="POST">
+            <form class="align-middle m-4" action="add-image" method="POST" enctype="multipart/form-data">
                 <nav aria-label="breadcrumb">
                     <ol class="breadcrumb">
                         <li class="breadcrumb-item"><a href="gallery">Galeria</a></li>
@@ -26,19 +26,30 @@
                 <div class="d-flex flex-column">
                     <div class="mb-3 col-12">
                         <label class="col-12">Titol de la imatge
-                            <input type="text" class="form-control col-12" id="title" name="title" placeholder="Títol de la imatge">
+                            <input type="text" class="form-control col-12" id="imageTitle" name="imageTitle" placeholder="Títol de la imatge">
                         </label>
                     </div>
                     <div class="mb-3 col-12">
                         <label class="col-12">Imatge
-                            <input class="form-control" type="file" accept="image/png, image/jpeg" id="formFile" onchange="setImagePreview(this)">
+                            <input class="form-control" id="imageFile" name="imageFile" type="file" accept="image/png, image/jpeg" onchange="setImagePreview(this)">
                         </label>
                     </div>
                     <div class="col-6 my-5 w-100 position-relative">
                         <img id="previewImage" class="img-fluid position-absloute border">
                         <span class="position-absolute translate-middle top-50 start-50">Previsualitzacio</span>
                     </div>
-                    <button type="submit" class="btn btn-primary mt-auto">Guardar Imatge</button>
+                    <?php
+                    if (!empty($alertMessage)) {
+                        echo "<div class=\"mt-3 mb-0 d-flex justify-content align-items-center alert alert-$alertType alert-dismissible\" role=\"alert\"><span class=\"me-3\">$alertIcon</span> $alertMessage <button type=\"button\" class=\"btn-close\" aria-label=\"Close\" data-bs-dismiss=\"alert\" ></button></div>";
+                    }
+                    if (isset($_SESSION["login-attempts"])) {
+                        if ($_SESSION["login-attempts"] > 2) {
+                            echo "<label class=\"mt-3\">Captcha</label>";
+                            echo "<div class=\"h-captcha\" data-sitekey=\"4d7d3404-e41b-4616-8035-8fd9b72cca7b\"></div>";
+                        }
+                    }
+                    ?>
+                    <button type="submit" name="submit" class="btn btn-primary mt-auto">Guardar Imatge</button>
                 </div>
             </form>
 
