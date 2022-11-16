9023b6a54a0c987874152ef120ecf98455af93a0
Marc Peral <m.peral@sapalomera.cat>
2022-11-16 22:27:00 +0100

    fix: update repo url when clicking version display

--- src/internal/viewFunctions/git.php
+++ src/internal/viewFunctions/git.php
@@ -19,6 +19,6 @@ function printGitInfo(string $commit): void
     $version = mb_substr($commit, 0, 5);
 
     echo "<footer class=\"fixed-bottom\">";
-    echo "<span class=\"text-muted py-1 px-1\"><a class=\"text-decoration-none \" href= \"https://github.com/mperalsapa/servidor-pt04/\">v-$version</a></span>";
+    echo "<span class=\"text-muted py-1 px-1\"><a class=\"text-decoration-none \" href= \"https://github.com/mperalsapa/servidor-pt05/\">v-$version</a></span>";
     echo "</footer>";
 }
